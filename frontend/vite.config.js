48a0473d532ab9d5a1624008b6a6391fa49a82ba
satyam122d <satyma.mohnaty.work@gmail.com>
2026-02-12 13:30:48 +0530

    Update Vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hospital-management-system/",
+  base: "/hospitalmanagementsystem/",
 })
